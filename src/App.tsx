3cc120a7902ae5ba24320bffed1d53492c3e55d1
adithyay19 <adithya842@gmail.com>
2024-07-25 10:39:58 +0530

    Added input section styling and created card body

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
 import { useState } from "react";
 
+const lists = [
+  ["one", "two", "three"],
+  ["four", "five"],
+  ["six", "seven", "eight", "nine"],
+];
+
+function Card(props: any) {
+  return(
+    <div className="card">
+      {lists[props.index].map((item) => <h4>{item}</h4>)}
+    </div>
+  );
+}
+
 
 function App() {
 
@@ -19,13 +33,19 @@ function App() {
       <div className="header">
         <h1>To-Do List</h1>
       </div>
-      <div>
-        <form onSubmit={handleSubmit}>
+      <div className="input-seion">
+        <form onSubmit={handleSubmit} className="input-section">
           <input type="text" placeholder="Task" value={task} onChange={(e) => setTask(e.target.value)} />
           <input type="date" placeholder="Task" value={date} onChange={(e) => setDate(e.target.value)} />
           <input type="time" placeholder="Task" value={time} onChange={(e) => setTime(e.target.value)} />
           <button>Add</button>
         </form>
+      </div>
+      <div className="cards-grid">
+        <ul>
+
+        {lists.map((l) => <Card index={lists.indexOf(l)}/>)}
+        </ul>
       </div>
     </div>
   )
