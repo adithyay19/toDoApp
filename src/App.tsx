59ea968e26ca1b3bb8ddd7d520976a02b1f5ae8c
adithyay19 <adithya842@gmail.com>
2024-07-25 08:10:12 +0530

    Input fields added

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [task, setTask] = useState('');
+  const [date, setDate] = useState('');
+  const [time, setTime] = useState('');
+ 
+  function handleSubmit(event: any) {
+    event.preventDefault();
+    console.log(task, date, time);
+    
+
+  }
 
   return (
-    <>
+    <div>
+      <div className="header">
+        <h1>To-Do List</h1>
+      </div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Task" value={task} onChange={(e) => setTask(e.target.value)} />
+          <input type="date" placeholder="Task" value={date} onChange={(e) => setDate(e.target.value)} />
+          <input type="time" placeholder="Task" value={time} onChange={(e) => setTime(e.target.value)} />
+          <button>Add</button>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
